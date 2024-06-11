bb7f879d8140b6d25c2649ee9ce56c24710cf6de
carl-zk <zxfspace@163.com>
2024-06-11 12:54:02 +0800

    bash

--- source/_posts/windows/sync.sh
+++ source/_posts/windows/sync.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 
 echo sync eclipse.pref setting.xml ... to blog
 
@@ -9,9 +9,11 @@ cp /c/Users/Carl/Documents/eclipse_preferences.epf /d/blog/source/_posts/windows
 cp /d/local/maven/maven/conf/settings.xml /d/blog/source/_posts/windows/
 cp /d/local/maven/maven/conf/toolchains.xml /d/blog/source/_posts/windows/
 cp /d/installed/coreDNS/Corefile /d/blog/source/_posts/windows/
-cp /d/local/eclise/eclipse.ini /d/blog/source/_posts/windows/
+cp /d/local/eclipse/eclipse.ini /d/blog/source/_posts/windows/
 
-echo done
+echo 'done. sync to git? (y/n)'
+read
+test 'y' = "$REPLY" || exit 0
 
 cd /d/blog
 git add .
