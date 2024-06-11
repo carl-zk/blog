801f2325b854eb578785544baedf225b2a5bb2c8
carl-zk <zxfspace@163.com>
2024-06-11 12:54:25 +0800

    auto-deploy

---
+++ 2024/05/19/windows/sync.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+
+echo sync eclipse.pref setting.xml ... to blog
+
+cp /c/Users/Carl/.bashrc /d/blog/source/_posts/windows/
+cp /c/Users/Carl/.bash_profile /d/blog/source/_posts/windows/
+cp /c/Users/Carl/sync.sh /d/blog/source/_posts/windows/
+cp /c/Users/Carl/Documents/eclipse_preferences.epf /d/blog/source/_posts/windows/
+cp /d/local/maven/maven/conf/settings.xml /d/blog/source/_posts/windows/
+cp /d/local/maven/maven/conf/toolchains.xml /d/blog/source/_posts/windows/
+cp /d/installed/coreDNS/Corefile /d/blog/source/_posts/windows/
+cp /d/local/eclipse/eclipse.ini /d/blog/source/_posts/windows/
+
+echo 'done. sync to git? (y/n)'
+read
+test 'y' = "$REPLY" || exit 0
+
+cd /d/blog
+git add .
+git commit -m"backup"
+git pull origin master
+git push origin master
+
